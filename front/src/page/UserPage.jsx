07b0e8a945dfd43c03752be34e1243caac851a34
duncan <costes.duncan@gmail.com>
2024-08-19 17:14:12 +0200

    front debut du front user

--- front/src/page/UserPage.jsx
+++ front/src/page/UserPage.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 export default function UserPage() {
+    useState[users, setUsers] = useState();
+
+
+    useEffect(()=>{
+        axios.get("http://localhost:8000/users")
+        .then(res => setUsers(res.data))
+        .catch(err=> console.log(err))
+    },[])
+
   return (
     <div>UserPage</div>
   )
