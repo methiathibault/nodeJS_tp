99d379354a172ddfac4db9ecb1d5977ada72226f
duncan <costes.duncan@gmail.com>
2024-08-20 09:18:17 +0200

    front face de la page user

--- front/src/page/UserPage.jsx
+++ front/src/page/UserPage.jsx
@@ -3,16 +3,49 @@ import { useState,useEffect } from 'react'
 import axios from 'axios'
 
 export default function UserPage() {
-    useState[users, setUsers] = useState();
+    const [users, setUsers] = useState([]);
+    const [newUserName, setNewUserName] = useState("")
+    const [newUserPass, setNewUserPass] = useState("")
 
+    function changeUsername (){
+
+    }
+
+    function changePassword(){
+
+    }
+
+    function removeUser(){
+
+    }
+    
+    function addUser(){
+
+    }
 
     useEffect(()=>{
-        axios.get("http://localhost:8000/users")
+        axios.get("http://localhost:8000/users/")
         .then(res => setUsers(res.data))
         .catch(err=> console.log(err))
     },[])
 
   return (
-    <div>UserPage</div>
+    <div>
+      <div>UserPage</div>
+
+      <div>
+        {users.map((element) =>(
+
+          <div>
+             <input value={element.username} onChange={(e)=> changeUsername(e.target.value)}/>  -  <input value={element.username} onChange={(e)=> changeUsername(e.target.value)}/>  <button onClick={()=> removeUser()}> delete </button>
+          </div>
+        ))}
+
+          <div>
+             ajout
+             <input onChange={(e)=> setNewUserName(e.target.value)}/>  -  <input onChange={(e)=> setNewUserPass(e.target.value)}/>  <button onClick={()=>addUser()}> ajout </button>
+          </div>
+      </div>
+    </div>
   )
 }
